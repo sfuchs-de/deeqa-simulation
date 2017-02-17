7ddf91202c7aa5e235ad578f69814bac7110b64a
Lucks 87 <sfuchs.de@gmail.com>
2017-02-17 16:55:16 +0100

    Fix in static equilibrium; wage loop did not converge because of wrong assignment of the fixed cost matrix; now correct and converges;

--- main.m
+++ main.m
@@ -38,7 +38,7 @@ for i=1:nregions,
     varphi(:,:,i) = lognrnd(.5,.1,nfirms,nsector);
 end
 
-beta=.8;
+beta=8;
 eta=.7; %CD Share of Blue collar workers in production
 delta=.1; % CD Share of experience in marketing
 gamma=.1; % CD Share of blue collar in marketing
@@ -218,37 +218,61 @@ end
 
 %checked and correct
 
+
+
+
 %% LABOR MOBILITY DECISION
 
 %Configuration
-tau_sec=rand(nsector,nsector)+1;
-tau_geo=rand(nregions,nregions)+1;
+tau_sec=rand(nsector,nsector)+10;
+tau_geo=rand(nregions,nregions)+10;
 for i=1:nregions,
     tau_geo(i,i)=0;
 end
-nu=1.5;
+for i=1:nsector,
+    tau_sec(i,i)=0;
+end
+nu=4;
 beta=0.9;
 
 nworkers_blue_old=nworkers_blue;
 nworkers_white_old=nworkers_white;
 
-%Run for each occupation type
-[nworkers_blue,nworkers_blue_ind] = LaborLoop_adj(wage_blue, nworkers_blue, tau_sec, tau_geo, nu, beta,nregions,nsector);
-[nworkers_white,nworkers_white_ind] = LaborLoop_adj(wage_white, nworkers_white-sum(nworkers_exp,3), tau_sec, tau_geo, nu, beta,nregions,nsector);
-for i=1:ndest,
-    [nworkers_exp(:,:,i),nworkers_white_exp_ind(:,:,:,:,i)] = LaborLoop_adj(wage_white+wage_exp(:,:,i), nworkers_exp(:,:,i), tau_sec, tau_geo, nu, beta,nregions,nsector);
-end
 
+%Expected wages have to be consistent with general equilibrium future wages
+error=6;
+nworkers_blue_temp=nworkers_blue;
+nworkers_white_temp=nworkers_white;
+nworkers_exp_temp=nworkers_exp;
+while error>.01,
+    [nworkers_blue_temp,nworkers_blue_ind] = LaborLoop_adj(wage_blue, nworkers_blue, tau_sec, tau_geo, nu, beta,nregions,nsector);
+    [nworkers_white_temp,nworkers_white_ind] = LaborLoop_adj(wage_white, nworkers_white-sum(nworkers_exp,3), tau_sec, tau_geo, nu, beta,nregions,nsector);
+    for i=1:ndest,
+        [nworkers_exp_temp(:,:,i),nworkers_white_exp_ind(:,:,:,:,i)] = LaborLoop_adj(wage_white+wage_exp(:,:,i), nworkers_exp(:,:,i), tau_sec, tau_geo, nu, beta,nregions,nsector);
+    end
+    Model.nworkers_white=.7*Model.nworkers_white+.3*(nworkers_white_temp+sum(nworkers_exp_temp,3));
+    Model.nworkers_blue=.7*Model.nworkers_blue+.3*nworkers_blue_temp;
+    Model.nworkers_exp=.7*Model.nworkers_exp+.3*nworkers_exp_temp;
+    wage_blue_old=wage_blue;
+    [wage_blue,wage_white,wage_exp,quant_prod,nworkers_exp_upd,lab_demand_white] = Static_Eq(Model);
+    error=sum(sum(abs(wage_blue_old-wage_blue)))
+end
 
 Model.nworkers_white=nworkers_white+sum(nworkers_exp,3);
 nworkers_white=nworkers_white+sum(nworkers_exp,3);
 Model.nworkers_blue=nworkers_blue;
 Model.nworkers_exp=nworkers_exp;
 [wage_blue,wage_white,wage_exp,quant_prod,nworkers_exp_upd,lab_demand_white] = Static_Eq(Model);
+nworkers_exp_upd
+
+
 Model.nworkers_exp=nworkers_exp_upd;
+
 [wage_blue,wage_white,wage_exp,quant_prod] = Static_Eq(Model); %Adjust wages to reflect knowledge creation
 
 
+
+% Storage of results
 worker_panel_store_2=worker_panel_blue(:,:,1);
 for i=1:nregions,
     for j=1:nregions,
@@ -318,6 +342,7 @@ for i=1:nregions,
 end
 worker_panel_white(:,:,1)=worker_panel_store;
 clear worker_panel_store worker_panel_store_2;
+
 
 
 worker_panel_store=worker_panel_white(:,:,1);
@@ -401,7 +426,10 @@ end
 test_obj-abs(nworkers_exp-nworkers_exp_old)
 clear test_obj
 
-
+%Problem: Only 1 experienced worker for 1,2,1 but wants 3 - nworkers_exp
+%inconsistent with worker_panel_white(:,:,2) Could be due to previous loop
+%adjusting the numbers or nworkers_exp labor adjustment loop getting the
+%numbers wrong.
 for j=1:nregions,
     for l=1:nsector,
         for n=1:ndest,
@@ -453,6 +481,7 @@ nworkers_white=nworkers_white+sum(nworkers_exp,3);
 Model.nworkers_blue=nworkers_blue;
 Model.nworkers_exp=nworkers_exp;
 [wage_blue,wage_white,wage_exp,quant_prod,nworkers_exp_upd,lab_demand_white] = Static_Eq(Model);
+nworkers_exp_upd
 Model.nworkers_exp=nworkers_exp_upd;
 [wage_blue,wage_white,wage_exp,quant_prod] = Static_Eq(Model); %Adjust wages to reflect knowledge creation
 
